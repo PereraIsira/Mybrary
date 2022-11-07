39fc80eb1f26a3bae95e5a3c3dfb194715e5ab28
iaperera <iaperera@iu.edu>
2022-11-06 20:54:00 -0600

    11/6/22 episode 2

---
+++ routes/authors.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All Authors Route
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+// New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', {author: new Author()})
+})
+
+router.post('/', (req,res) => {
+    res.send('create')
+})
+
+module.exports = router
